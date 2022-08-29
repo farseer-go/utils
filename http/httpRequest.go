39f243dd7efdfe5d7ad696e1fe1a8cfc296f801a
steden <1470804@qq.com>
2022-08-30 01:44:03 +0800

    移除旧代码

--- http/httpRequest.go
+++ http/httpRequest.go
@@ -43,22 +43,4 @@ func httpRequest(methodName string, url string, head map[string]any, body any, c
 		panic(err.Error())
 	}
 	return string(response.Body())
-	/*
-		// 原生
-		client := http.Client{
-				Timeout: time.Duration(requestTimeout) * time.Millisecond,
-			}
-			bytesData, _ := json.Marshal(body)
-			request, _ := http.NewRequest("GET", url, bytes.NewReader(bytesData))
-			if contentType != "" {
-				request.Header.Set("Content-Type", contentType)
-			}
-			resp, err := client.Do(request)
-			defer resp.Body.Close()
-			if err != nil {
-				panic(err.Error())
-			}
-			rspBody, _ := ioutil.ReadAll(resp.Body)
-			return string(rspBody)
-	*/
 }
