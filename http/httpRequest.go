123dec0acf71239739a5c1ef73d95be74ece28b6
steden <1470804@qq.com>
2022-09-11 21:22:50 +0800

    为各组件，增加日志打印配置

--- http/httpRequest.go
+++ http/httpRequest.go
@@ -4,12 +4,15 @@ import (
 	"encoding/json"
 	"github.com/farseer-go/fs/flog"
 	"github.com/farseer-go/fs/parse"
+	"github.com/farseer-go/fs/stopwatch"
 	"github.com/valyala/fasthttp"
 	"time"
 )
 
 // 支持请求超时设置，单位：ms
 func httpRequest(methodName string, url string, head map[string]any, body any, contentType string, requestTimeout int) (string, error) {
+	sw := stopwatch.StartNew()
+
 	client := fasthttp.Client{}
 
 	// request
@@ -38,8 +41,9 @@ func httpRequest(methodName string, url string, head map[string]any, body any, c
 
 	response := fasthttp.AcquireResponse()
 	timeout := time.Duration(requestTimeout) * time.Millisecond
-	flog.AppInfof("httpRequest", "[%s] %s body:%s", methodName, url, string(bytesData))
 	err := client.DoTimeout(request, response, timeout)
+
+	flog.ComponentInfof("httpRequest", "[%s] %s body:%s，耗时：%s", methodName, url, string(bytesData), sw.GetMillisecondsText())
 
 	if err != nil {
 		flog.Errorf("%s request error:%s", url, err.Error())
