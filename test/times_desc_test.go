99be1a062935113825d3c6ab5b5b9ee4c5773a44
yangwei <123576128@qq.com>
2023-01-04 00:15:00 +0800

    提高测试覆盖率

--- test/times_desc_test.go
+++ test/times_desc_test.go
@@ -12,5 +12,20 @@ func TestGetSubDesc(t *testing.T) {
 	ts2 := time.Date(2022, 8, 1, 19, 22, 12, 0, time.Local)
 
 	desc := times.GetSubDesc(ts1, ts2)
-	assert.Equal(t, desc, "1 小时 32 分")
+	assert.Equal(t, "1 小时 32 分", desc)
+
+	ts1 = time.Date(2022, 8, 2, 20, 54, 12, 0, time.Local)
+	ts2 = time.Date(2022, 8, 1, 19, 22, 12, 0, time.Local)
+	desc = times.GetSubDesc(ts1, ts2)
+	assert.Equal(t, "1 天 1 小时", desc)
+
+	ts1 = time.Date(2022, 8, 1, 19, 54, 19, 0, time.Local)
+	ts2 = time.Date(2022, 8, 1, 19, 22, 12, 0, time.Local)
+	desc = times.GetSubDesc(ts1, ts2)
+	assert.Equal(t, "32 分 7 秒", desc)
+
+	ts1 = time.Date(2022, 8, 1, 19, 22, 19, 99, time.Local)
+	ts2 = time.Date(2022, 8, 1, 19, 22, 12, 0, time.Local)
+	desc = times.GetSubDesc(ts1, ts2)
+	assert.Equal(t, "7 秒", desc)
 }
