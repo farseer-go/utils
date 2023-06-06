c88883282959d83ae8a1ce40decb830a70724170
steden <1470804@qq.com>
2023-06-06 23:12:00 +0800

    interface{} ==> any

--- test/http_post_test.go
+++ test/http_post_test.go
@@ -40,13 +40,13 @@ func TestPost(t *testing.T) {
 }
 
 func TestPostForm(t *testing.T) {
-	data := make(map[string]interface{})
+	data := make(map[string]any)
 	data["name"] = "zhaofan"
 	data["age"] = "23"
 	res, statusCode, err := http.PostForm("https://httpbin.org/post", nil, data, 5000)
 	assert.NoError(t, err)
 	assert.Equal(t, fasthttp.StatusOK, statusCode)
-	var val = make(map[string]interface{}, 0)
+	var val = make(map[string]any, 0)
 	err = json.Unmarshal([]byte(res), &val)
 	assert.NoError(t, err)
 	assert.Equal(t, data, val["form"])
@@ -56,13 +56,13 @@ func TestPostFormWithoutBody(t *testing.T) {
 	res, statusCode, err := http.PostFormWithoutBody("https://httpbin.org/post", nil, 5000)
 	assert.NoError(t, err)
 	assert.Equal(t, fasthttp.StatusOK, statusCode)
-	var val = make(map[string]interface{}, 0)
+	var val = make(map[string]any, 0)
 	err = json.Unmarshal([]byte(res), &val)
 	assert.NoError(t, err)
 }
 
 func TestPostJson(t *testing.T) {
-	data := make(map[string]interface{})
+	data := make(map[string]any)
 	data["name"] = "zhaofan"
 	data["age"] = "23"
 
