599abc16526865f04b595c11391b45f6b4ad92ea
steden <1470804@qq.com>
2023-10-11 22:05:07 +0800

    添加常用工具方法

--- operator/math.go
+++ operator/math.go
@@ -17,15 +17,18 @@ func GetSum(num int) int {
 	return sum
 }
 
-// GetTail 取尾数
+// GetTail 取数字的个位数
 func GetTail(num int) int {
 	//如果数值是个位数直接返回当前数值
 	if num < 10 {
 		return num
 	}
 	strNum := parse.Convert(num, "")
-	if len(strNum) == 0 {
-		return 0
-	}
 	return parse.Convert(strNum[(len(strNum)-1):], 0)
+}
+
+// GetHead 取数字第一位数
+func GetHead(num int) int {
+	strNum := parse.Convert(num, "")
+	return parse.ToInt(strNum[0])
 }
