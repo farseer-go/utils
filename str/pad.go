641a3ea2245d68f7ab4c881492f54573a15ae59d
steden <1470804@qq.com>
2023-12-26 19:19:19 +0800

    恢复string

--- str/pad.go
+++ str/pad.go
@@ -3,19 +3,17 @@ package str
 import "strings"
 
 // PadLeft 为str向左填充paddingChar，直到长度=totalWidth
-func PadLeft(str string, totalWidth int, paddingChar rune) string {
-	char := string(paddingChar)
+func PadLeft(str string, totalWidth int, paddingChar string) string {
 	if Length(str) >= totalWidth {
 		return str
 	}
-	return strings.Repeat(char, totalWidth-Length(str)) + str
+	return strings.Repeat(paddingChar, totalWidth-Length(str)) + str
 }
 
 // PadRight 为str向左填充paddingChar，直到长度=totalWidth
-func PadRight(str string, totalWidth int, paddingChar rune) string {
-	char := string(paddingChar)
+func PadRight(str string, totalWidth int, paddingChar string) string {
 	if Length(str) >= totalWidth {
 		return str
 	}
-	return str + strings.Repeat(char, totalWidth-Length(str))
+	return str + strings.Repeat(paddingChar, totalWidth-Length(str))
 }
