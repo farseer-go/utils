60769017dd4bd5e92239fd546aef10e8bb189bd9
steden <1470804@qq.com>
2024-01-08 17:36:29 +0800

    修复测试错误

--- test/str_pad_test.go
+++ test/str_pad_test.go
@@ -7,11 +7,11 @@ import (
 )
 
 func TestPadRight(t *testing.T) {
-	assert.Equal(t, str.PadRight("1", 5, []rune("0")[0]), "10000")
-	assert.Equal(t, str.PadRight("哈哈", 5, []rune("哼")[0]), "哈哈哼哼哼")
+	assert.Equal(t, str.PadRight("1", 5, "0"), "10000")
+	assert.Equal(t, str.PadRight("哈哈", 5, "哼"), "哈哈哼哼哼")
 }
 
 func TestPadLeft(t *testing.T) {
-	assert.Equal(t, str.PadLeft("1", 5, []rune("0")[0]), "00001")
-	assert.Equal(t, str.PadLeft("哈哈", 5, []rune("哼")[0]), "哼哼哼哈哈")
+	assert.Equal(t, str.PadLeft("1", 5, "0"), "00001")
+	assert.Equal(t, str.PadLeft("哈哈", 5, "哼"), "哼哼哼哈哈")
 }
