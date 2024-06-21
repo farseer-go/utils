02bcc07469f741fd553bfd5b70fdc2f3ca7bcfc8
steden <1470804@qq.com>
2024-06-21 22:46:51 +0800

    新增：sh1加密码

--- encrypt/md5.go
+++ encrypt/md5.go
@@ -2,6 +2,7 @@ package encrypt
 
 import (
 	"crypto/md5"
+	"crypto/sha1"
 	"fmt"
 )
 
@@ -11,4 +12,26 @@ import (
 func Md5(str string) string {
 	sum := md5.Sum([]byte(str))
 	return fmt.Sprintf("%x", sum) //将[]byte转成16进制
+}
+
+// Sha1 加密码
+func Sha1(str string) string {
+	// The pattern for generating a hash is `sha1.New()`,
+	// `sha1.Write(bytes)`, then `sha1.Sum([]byte{})`.
+	// Here we start with a new hash.
+	h := sha1.New()
+
+	// `Write` expects bytes. If you have a string `s`,
+	// use `[]byte(s)` to coerce it to bytes.
+	h.Write([]byte(str))
+
+	// This gets the finalized hash result as a byte
+	// slice. The argument to `Sum` can be used to append
+	// to an existing byte slice: it usually isn't needed.
+	bs := h.Sum(nil)
+
+	// SHA1 values are often printed in hex, for example
+	// in git commits. Use the `%x` format verb to convert
+	// a hash results to a hex string.
+	return fmt.Sprintf("%x", bs)
 }
--- test/md5_test.go
+++ test/md5_test.go
@@ -9,5 +9,11 @@ import (
 // CopyFolder
 func TestMd5(t *testing.T) {
 	s := encrypt.Md5("123")
-	assert.Equal(t, s, "202cb962ac59075b964b07152d234b70")
+	assert.Equal(t, "202cb962ac59075b964b07152d234b70", s)
+}
+
+// CopyFolder
+func TestSha1(t *testing.T) {
+	s := encrypt.Sha1("abc")
+	assert.Equal(t, "a9993e364706816aba3e25717850c26c9cd0d89d", s)
 }
