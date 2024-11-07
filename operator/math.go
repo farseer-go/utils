cf8c49daf43f2c0f69cd14fea8723e371d07f105
steden <1470804@qq.com>
2024-11-07 23:43:50 +0800

    算法调整

--- operator/math.go
+++ operator/math.go
@@ -7,14 +7,15 @@ import (
 
 // GetSum 取和数
 func GetSum(num int) int {
-	sum := 0
-	// 迭代每一位数字
-	for num != 0 {
-		digit := num % 10 // 每次取个位数
-		sum += digit      // 将个位数加到总和上
-		num /= 10         // 去掉个位数
-	}
-	return sum
+	return num/10 + num%10
+	// sum := 0
+	// // 迭代每一位数字
+	// for num != 0 {
+	// 	digit := num % 10 // 每次取个位数
+	// 	sum += digit      // 将个位数加到总和上
+	// 	num /= 10         // 去掉个位数
+	// }
+	// return sum
 }
 
 // GetTail 取数字的个位数
