de19cec34ac87f11f033e59d82264060e919f44a
steden <1470804@qq.com>
2024-11-19 13:31:40 +0800

    调整UT

--- test/http_download_test.go
+++ test/http_download_test.go
@@ -1,12 +1,13 @@
 package test
 
 import (
+	"testing"
+
 	"github.com/farseer-go/utils/http"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 func TestHttpDownload(t *testing.T) {
-	_, err := http.Download("https://github.com/farseers/FOPS-Actions/releases/download/v1/gitProxy", "./gitProxy", nil, 0, "socks5://127.0.0.1:7890")
+	_, err := http.Download("https://github.com/farseers/FOPS-Actions/releases/download/v1/gitProxy", "./gitProxy", nil, 0, "") // socks5://127.0.0.1:7890
 	assert.Nil(t, err)
 }
